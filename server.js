4d4f138f90ff152e9e5d63444209fe5aeb6e63e9
root <root@EOMUa1.(none)>
2014-12-09 11:03:27 +0800

    user

--- server.js
+++ server.js
@@ -12,43 +12,8 @@ console.log("server start on port 8080");
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.sendfile('public/main.html');
 });
-
-
-// rest api
-function getDevice() {
-
-  var options = {
-    hostname: '127.0.0.1',
-    port: 8888,
-    path: '/v2/catalog',
-    method: 'GET',
-    headers: {
-        'Content-type': 'application/json',
-      },
-    auth: 'admin:admin',
-  };
-
-  var req = http.request(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-    res.setEncoding('utf8');
-    res.on('data', function (data) {
-      console.log('BODY: ' + data);
-    });
-  });
-
-  req.on('error', function(err) {
-    console.log('request error: ' + err.message);
-  });
-
-  // write data to request body
-  //req.write('data\n');
-  //req.write('data\n');
-  req.end();
-}
 
 // for webrtc signalling
 var sessions = {};
@@ -77,37 +42,85 @@ io.on('connection', function (socket) {
   // tell client connected
   socket.emit('connect_ack');
 
-  socket.on('register', function(json) {
-    console.log('register: ' + json.name);
+  // update device info
+  //getDevices();
 
-    // add  client
-    client.name = json.name;
-    clients[client.name] = client;
+  socket.on('register', function(json) {
+    console.log('register: ' + JSON.stringify(json));
 
     var msg = {};
     msg['time'] = getTime();
     msg['color'] = client.color;
-    msg['name'] = client.name;
+    msg['name'] = json.name;
+
+    // add user
+    if (!addUser(json.name, json.password)) {
+      msg['result'] = 'invalid user';
+      sendMessage(client.socket,'register_ack', msg);
+      return;
+    }
+
     msg['result'] = 'ok';
     sendMessage(client.socket,'register_ack', msg);
   });
 
-  socket.on('invite', function(json) {
-    console.log('invite: ' + json.caller + ' call ' + json.callee);
+  socket.on('login', function(json) {
+    console.log('login: ' + JSON.stringify(json));
 
+    var msg = {};
+    msg['time'] = getTime();
+    msg['color'] = client.color;
+    msg['name'] = json.name;
+
+    // check user
+    if (!checkUser(json.name, json.password)) {
+      msg['result'] = 'invalid user';
+      sendMessage(client.socket,'login_ack', msg);
+      return;
+    }
+
+    // check device
+/*    if (!checkDevice(client.ip)) {
+      msg['result'] = 'invalid device';
+      sendMessage(client.socket,'login_ack', msg);
+      return;
+    }*/
+
+    // add client
+    client.name = json.name;
+    clients[client.name] = client;
+
+    msg['result'] = 'ok';
+    sendMessage(client.socket,'login_ack', msg);
+  });
+
+  socket.on('invite', function(json) {
+    console.log('invite: ' + JSON.stringify(json));
+
+    // call callee
     var msg = json;
     msg['time'] = getTime();
     msg['color'] = client.color;
+
     if (clients[json.callee]) {
+
+      //check device
+/*      if (!checkDevice(clients[json.callee].ip)) {
+        msg['result'] = 'invalid device';
+        sendMessage(clients[json.caller].socket,'invite_ack', msg);
+        return;
+      }*/
+
       sendMessage(clients[json.callee].socket,'invite', msg);
+
     } else if (clients[json.caller]) {
-      msg['result'] = 'no exist';
+      msg['result'] = 'not exist';
       sendMessage(clients[json.caller].socket,'invite_ack', msg);
     }
   });
 
   socket.on('invite_ack', function(json) {
-    console.log('invite_ack: ' + json.caller + ' call ' + json.callee);
+    console.log('invite_ack: ' + JSON.stringify(json));
     
     if (json.result === 'ok') {
       // add session
@@ -126,7 +139,7 @@ io.on('connection', function (socket) {
   });
 
   socket.on('message', function(json) {
-    console.log('message: '+ ": " + client.name + " " + json.text);
+    console.log('message: '+ JSON.stringify(json));
 
     // notice session members
     var msg = json;
@@ -212,3 +225,82 @@ var getColor=function(){
                 'orange','blue','blueviolet','brown','burlywood','cadetblue'];
   return colors[Math.round(Math.random() * 10000 % colors.length)];
 }
+
+// user api
+var users = {};
+
+var addUser = function(name, password) {
+
+  users[name] = password;
+
+  return true;
+}
+
+var checkUser = function(name, password) {
+
+  if (users[name] === password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// sweet api
+
+var devices = {};
+
+function getDevices()  {
+  var options = {
+    hostname: '12.12.12.134',
+    port: 8080,
+    path: '/controller/nb/v2/sweet/default/devices',
+    method: 'GET',
+    headers: {
+        'Content-type': 'application/json',
+      },
+    auth: 'admin:admin',
+  };
+
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+    res.setEncoding('utf8');
+    res.on('data', function (data) {
+      console.log('BODY: ' + data);
+
+      // get devices info
+      devices = JSON.parse(data);
+    });
+  });
+
+  req.on('error', function(err) {
+    console.log('request error: ' + err.message);
+  });
+
+  // write data to request body
+  //req.write('data\n');
+  //req.write('data\n');
+  req.end();
+}
+
+var checkDevice = function(ip) {
+
+  var json = devices;
+
+  console.log('checkDevice: '+ ip + '\r\n' + JSON.stringify(json['devicemaplist']));
+
+  for (var i in json['devicemaplist']) {
+    console.log(JSON.stringify(json['devicemaplist'][i]));
+    console.log(JSON.stringify(json['devicemaplist'][i]['devicemap']));
+    console.log(JSON.stringify(json['devicemaplist'][i]['devicemap']['deviceIp']));
+
+    if (json['devicemaplist'][i]['devicemap']['deviceIp'] === ip) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
